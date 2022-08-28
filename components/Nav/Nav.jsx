c970a0f9a695f7e3c677cfbfc2092875a5133622
James Sullivan <jsullivan2847@gmail.com>
2022-08-28 19:55:54 -0400

    tailwind installed

--- components/Nav/Nav.jsx
+++ components/Nav/Nav.jsx
@@ -5,11 +5,13 @@ import styles from '/styles/Layout/Nav/Nav.module.css'
 
 export default function Nav() {
   return (
-    <nav className={styles.nav}>
-        <a href="/About"><h3>About</h3></a>
-        <a href=""><h3>Skills</h3></a>
-        <a href=""><h3>Projects</h3></a>
-        <a href=""><h3>Experience</h3></a>
+    <nav className='flex-col p-5 w-fit'>
+        <a href="/About"><h3 className='transition-all duration-200 p-1 hover:mb-2 hover:bg-white hover:text-black rounded-md'>About</h3></a>
+        <a href="/Skills"><h3 className='transition-all p-1 hover:mb-2 hover:bg-white hover:text-black rounded-md' >Skills</h3></a>
+        <a href="/Projects"><h3 className='transition-all p-1 hover:mb-2 hover:bg-white hover:text-black rounded-md'>Projects</h3></a>
+        <a href="/Experience"><h3 className='transition-all p-1 hover:mb-2 hover:bg-white hover:text-black rounded-md'>Experience</h3></a>
     </nav>
   )
 }
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'components/Nav/Nav.jsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
